df75d6104fa27dd5ef267a101e7ab876fea5e977
OgliariNatan <natanogliari@gmail.com>
2023-04-04 09:44:10 -0300

    OO
    Orientada a objetos

---
+++ OO.py
@@ -0,0 +1,25 @@
+import math
+
+'''
+    Para exercicio de orirntado a objetos
+'''
+
+class Cachorro:
+
+    def __int__(self, nome, idade, raca):
+        self.nome = nome
+        self.idade = idade
+        self.raca = raca
+meu_cachorro = Cachorro()
+
+
+class Circulo:
+    def __int__(self):
+        self.raio = raio
+meu_circulo = Circulo()
+
+
+meu_cachorro.nome = "Toby"
+meu_cachorro.idade = 10
+meu_cachorro.raca = "Vira Lata"
+print(meu_cachorro.nome, meu_cachorro.idade, meu_cachorro.raca)
