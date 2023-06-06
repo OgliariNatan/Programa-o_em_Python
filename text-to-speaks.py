771abfc2900825d2eb8ef7164e87c2056cfcce52
OgliariNatan <natanogliari@gmail.com>
2023-06-06 10:35:29 -0300

    alterado a velociade e volume

--- text-to-speaks.py
+++ text-to-speaks.py
@@ -2,16 +2,17 @@ import pyttsx3
 
 # init mecanismo TTS
 engine = pyttsx3.init()
+# velociade da fala padrão 200
+engine.setProperty("rate", 400)
+
+# Volume, padrão 1.0
+engine.setProperty("volume", 0.5)
 
 #text to he speaks
 text = "Bem vindo a conversação em python\n Um pão de batata muito louco. \n A prática leva a perfeição"
 
 #to speak
-
 engine.say(text)
-
-#sleep
-
 engine.runAndWait()
 
 print("Concluida a conversa\n")
