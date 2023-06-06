d5290105e2e6ad44e0e561b2800ea2be8623075e
OgliariNatan <natanogliari@gmail.com>
2023-06-06 11:45:00 -0300

    jg

--- text-to-speaks.py
+++ text-to-speaks.py
@@ -1,13 +1,23 @@
 import pyttsx3
+import time
 
 # init mecanismo TTS
 engine = pyttsx3.init()
+
+# Obtenha a lista de vozes disponíveis e selecione uma
+voices = engine.getProperty("voices")
+for voice in voices:
+    print(voice.id)
+
+# Defina a voz para "brazil"
+engine.setProperty("voice", "brazil")
+
 # velociade da fala padrão 200
-engine.setProperty("rate", 400)
+engine.setProperty("rate", 250)
 
 # Volume, padrão 1.0
-engine.setProperty("volume", 0.5)
-
+engine.setProperty("volume", 1.5)
+time.sleep(2)
 #text to he speaks
 text = "Bem vindo a conversação em python\n Um pão de batata muito louco. \n A prática leva a perfeição"
 
